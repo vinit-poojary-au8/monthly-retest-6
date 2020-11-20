201a0d5853a861ca07ab7a31c53affc223777f63
vinit-poojary-au8 <vinitpoojary00@gmail.com>
2020-11-20 14:09:18 +0530

    trello project

---
+++ src/reducers/cardsReducer.js
@@ -0,0 +1,38 @@
+import { CONSTANTS } from "../actions";
+
+const initialState = {
+  
+};
+
+const cardsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CONSTANTS.ADD_CARD: {
+      const { text, listID, id } = action.payload;
+
+      const newCard = {
+        text,
+        id: `card-${id}`,
+        list: listID
+      };
+
+      return { ...state, [`card-${id}`]: newCard };
+    }
+    case CONSTANTS.EDIT_CARD: {
+      const { id, newText } = action.payload;
+      const card = state[id];
+      card.text = newText;
+      return { ...state, [`card-${id}`]: card };
+    }
+
+    case CONSTANTS.DELETE_CARD: {
+      const { id } = action.payload;
+      const newState = state;
+      delete newState[id];
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default cardsReducer;
